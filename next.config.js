97b746f91e5811f7ee0f4cdc992644a54f0543cb
Toni Karnas <105968622+blankzsh@users.noreply.github.com>
2023-08-03 00:47:25 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:dog*',
+        destination: 'https://cdn.houi.cn/:dog*',
+      },
+    ]
+  },
 }
